d1e8831512dbf37ac7a38329d5d93a3d657c18c5
mmejiar <mario_mejram@hotmail.com>
2016-01-08 11:27:23 -0600

    08/01/2016
    Se agrega controller y repository de busqueda de flotillas

---
+++ app/static/AngularJS/Busqueda/busquedaController.js
@@ -0,0 +1,26 @@
+﻿registrationModule.controller("busquedaController", function ($scope, $rootScope, localStorageService, alertFactory, busquedaRepository) {
+
+    //Grupo de funciones de inicio
+    $scope.init = function () {
+
+    };
+
+    //Botón obtener la flotilla dependiendo de la factura o vin
+    $scope.buscarFlotilla = function(factura,vin){
+        busquedaRepository.getFlotilla(factura,vin)
+            .success(getFlotillaSuccessCallback)
+            .error(errorCallBack);
+    };
+
+    //Succes obtiene lista de objetos de las flotillas
+    var getFlotillaSuccessCallback = function (data, status, headers, config) {
+        $rootScope.listaFlotillas = data;
+        alertFactory.success('Datos de flotillas cargados.');
+    };
+    
+    //Mensajes en caso de error
+    var errorCallBack = function (data, status, headers, config) {
+        alertFactory.error('No se encuentran flotillas con los datos de busqueda: ' + data);
+    };
+
+});
---
+++ app/static/AngularJS/Busqueda/busquedaRepository.js
@@ -0,0 +1,9 @@
+﻿var searchUrl = global_settings.urlCORS + '/api/usuarioApi/';
+
+registrationModule.factory('busquedaRepository', function ($http) {
+    return {
+        getFlotilla: function (factura, vin) {
+            return $http.get(searchUrl + '1|' + factura + vin);
+        }
+    };
+});
