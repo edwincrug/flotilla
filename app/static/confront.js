895d76fbbcab34fd013fcd57f1d85657fec0609d
mmejiar <mario_mejram@hotmail.com>
2016-01-15 16:13:36 -0600

    cambios

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,4 +1,5 @@
 var global_settings = {
-	urlCORS: 'http://192.168.20.9/ApiFlotilla',
-	uploadPath: 'C:/Desarrollo/Central/centralizacion/app/uploads'
+	/*urlCORS: 'http://192.168.20.9/ApiFlotilla',*/
+	urlCORS: 'http://localhost/8888',
+	uploadPath: 'C:/Desarrollo/Flotilla/flotilla/app/uploads/'
 } 	
