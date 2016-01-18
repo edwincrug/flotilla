654403d9184a5e8a932a20c31f75553eb00b10af
Vladimir Juarez <vladimouse@gmail.com>
2016-01-18 12:58:59 -0600

    numExt

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,4 +1,5 @@
 var global_settings = {
-	urlCORS: 'http://192.168.20.9/ApiFlotilla',
+	//urlCORS: 'http://192.168.20.9/ApiFlotilla',
+	urlCORS: 'http://localhost:61093/',
 	uploadPath: 'C:/Desarrollo/Flotilla/flotilla/app/uploads/'
 } 	
